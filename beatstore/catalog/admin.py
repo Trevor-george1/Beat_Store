e8cb36186746c540e3aba4449d56c1286d822bd5
Trevor-george1 <Trevormahinda@gmail.com>
2025-06-13 15:21:14 +0300

    audio track model

--- beatstore/catalog/admin.py
+++ beatstore/catalog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import AudioTrack
 # Register your models here.
+
+admin.site.register(AudioTrack)
--- beatstore/catalog/models.py
+++ beatstore/catalog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
 
 # Create your models here.
+
+class AudioTrack(models.Model):
+    CATEGORY_CHOICES = [
+        ('GENGETONE', 'gengetone'),
+        ('Hip Hop', 'rap'),
+        ('AFRO', 'afro'),
+        ('OTHER', 'other'),
+    ]
+
+    title = models.CharField(max_length=200)
+    audio_file = models.FileField(
+        upload_to='audio/',
+        validators=[FileExtensionValidator(allowed_extensions=['mp3', 'wav', 'ogg'])]
+    )
+
+    category = models.CharField(
+        max_length=20,
+        choices=CATEGORY_CHOICES,
+        default='OTHER'
+    )
+
+    cover_image = models.ImageField(upload_to='covers/', blank=True, null=True)
+    tempo = models.CharField(max_length=20)
+    key = models.CharField(max_length=20)
+    duration = models.CharField(max_length=20, blank=True)
+    upload_date = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.title}"
+
